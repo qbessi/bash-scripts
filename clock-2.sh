07f61ef8c38a69f2b578a5977623d765031da482
MK <mk@null.local>
2024-09-27 17:59:54 +0100

    task added

---
+++ clock-2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Clock shows h hours, m minutes and s seconds after midnight.
+
+#Your task is to write a function which returns the time since midnight in milliseconds.
+#Example:
+
+#h = 0
+#m = 1
+#s = 1
+
+#result = 61000
+
+h=$(($1 * 60 *  60 * 1000))
+m=$(($2 * 60 * 1000))
+s=$(($3 * 1000))
+result=$((h + m + s))
+
+function sinceMidnight() {
+    echo $result
+}
+
+sinceMidnight "$1"
