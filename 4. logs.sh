1484615c9ec574e708dc775cca02e2a3b9e5d19a
Qbessi <github@bessi.io>
2024-10-23 23:58:14 +0100

    update logs script

--- 4. logs.sh
+++ 4. logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ".::          .::::        .::::     .:: ::  "
+echo ".::           .::::        .::::     .:: ::  "
 echo ".::        .::    .::   .:    .:: .::    .::"
 echo ".::      .::        .::.::         .::      "
 echo ".::      .::        .::.::           .::    "
@@ -11,3 +11,11 @@ echo "--------------------------------------------"
 echo "Move old logs to an archive."
 echo "--------------------------------------------"
                                             
+LOGS="/var/log"
+FOLDER="archive"
+TIMESTAMP=$(date +"%Y-%m-%d_%H%M%S")
+
+sudo mkdir $LOGS/$FOLDER
+sudo find $LOGS -name "*.log" -type f -exec gzip {} \;
+sudo mv $LOGS/*.gz $LOGS/$FOLDER
+sudo rm -rf "*.log"
