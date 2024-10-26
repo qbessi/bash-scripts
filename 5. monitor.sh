1b59a86e904e3844ee3f0e08400bc2edbfb05257
Qbessi <github@bessi.io>
2024-10-26 14:58:59 +0100

    5 script fixes

--- 5. monitor.sh
+++ 5. monitor.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
-
-echo "╔╦╗┌─┐┌┐┌┬┌┬┐┌─┐┬─┐
-║║║│ │││││ │ │ │├┬┘
-╩ ╩└─┘┘└┘┴ ┴ └─┘┴└─"
+echo "╔╦╗┌─┐┌┐┌┬┌┬┐┌─┐┬─┐"
+echo "║║║│ │││││ │ │ │├┬┘"
+echo "╩ ╩└─┘┘└┘┴ ┴ └─┘┴└─"
 echo "--"
 echo "Monitor disk space and send alert via email."
 
 #!/bin/bash
 
-# Prompt for email and disk space to monitor
 read -p "Email to send alert to: " email_address
 read -p "Set Disk Space to Monitor (e.g., /dev/sda2): " monitor_disk
 
 while true; do
-# Check the specified mount point for usage
+
 usage=$(df | awk -v disk="$monitor_disk" '$1 == disk {print $5}' | tr -d '%')
 
-# Check if usage is a valid integer
 if [ "$usage" -ge 0 ] 2>/dev/null; then
     if [ "$usage" -ge 95 ]; then
         echo "Warning: Disk space for $monitor_disk is below 5% free!"
@@ -28,5 +24,5 @@ if [ "$usage" -ge 0 ] 2>/dev/null; then
 else
     echo "Error: Unable to retrieve disk usage for $monitor_disk."
 fi
-sleep 60
+    sleep 60
 done
